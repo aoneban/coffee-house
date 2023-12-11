59f42165b255e7560a58b25a9d4618ea7ffd534e
aoneban <torgregion1@gmail.com>
2023-12-11 23:26:21 +0100

    feat: add modal windows at 380 pixels

--- menu/scripts/modal.js
+++ menu/scripts/modal.js
@@ -1,6 +1,6 @@
 import { data } from '../../static/data.js';
 
-const modal = document.querySelector('.modal')
+const modal = document.querySelector('.modal');
 
 export function modalWindowGenerator(event) {
   document.body.style.position = 'fixed';
@@ -9,9 +9,9 @@ export function modalWindowGenerator(event) {
   const result = data.filter((el) => el.id == exampleAttr);
   modal.setAttribute('id', 'myModal');
   modal.style.display = 'flex';
-  modal.addEventListener('click', function(event) {
+  modal.addEventListener('click', function (event) {
     deleteWrap(event);
-  })
+  });
   const modalWrapper = document.createElement('div');
   modalWrapper.classList.add('modal-wrapper');
   modalWrapper.innerHTML = `
@@ -26,30 +26,30 @@ export function modalWindowGenerator(event) {
       <p class="modal-size">Size</p>
       <div class="buttons-size">
           <div class="button-container">
-              <input type="button" class="btn-size" onclick="alert('Hello World!')" value="200 ml">
+              <input type="button" class="btn-size active-button" value="${result[0].sizes.s.size}" checked>
               <span class="size-inside">S</span>
           </div>
           <div class="button-container">
-              <input type="button" class="btn-size" onclick="alert('Hello World!')" value="300 ml">
+              <input type="button" class="btn-size" value="${result[0].sizes.m.size}">
               <span class="size-inside">M</span>
           </div>
           <div class="button-container">
-              <input type="button" class="btn-size" onclick="alert('Hello World!')" value="400 ml">
+              <input type="button" class="btn-size" onclick="alert('Hello World!')" value="${result[0].sizes.l.size}">
               <span class="size-inside">L</span>
           </div>
       </div>
       <p class="additives">Additives</p>
       <div class="buttons-add">
           <div class="button-container">
-              <input type="button" class="btn-size" onclick="alert('Hello World!')" value="Sugar">
+              <input type="button" class="btn-size" onclick="alert('Hello World!')" value="${result[0].additives[0].name}">
               <span class="size-inside">1</span>
           </div>
           <div class="button-container">
-              <input type="button" class="btn-size" onclick="alert('Hello World!')" value="Cinnamon">
+              <input type="button" class="btn-size" onclick="alert('Hello World!')" value="${result[0].additives[1].name}">
               <span class="size-inside">2</span>
           </div>
           <div class="button-container">
-              <input type="button" class="btn-size" onclick="alert('Hello World!')" value="Syrup">
+              <input type="button" class="btn-size" onclick="alert('Hello World!')" value="${result[0].additives[2].name}">
               <span class="size-inside">3</span>
           </div>
       </div>
@@ -62,18 +62,18 @@ export function modalWindowGenerator(event) {
       </p>
       <button type="button" class="close-button">Close</button>
   </div>
-  `
+  `;
   console.log(result);
 
   modal.append(modalWrapper);
   document.body.append(modal);
 }
 
-function deleteWrap(event) {
+const deleteWrap = (event) => {
   if (event.target == modal) {
     const modalWrap = document.querySelector('.modal-wrapper');
     document.querySelector('.modal').style.display = 'none';
     document.body.style.position = '';
-    modalWrap.remove()
+    modalWrap.remove();
   }
-}
+};
