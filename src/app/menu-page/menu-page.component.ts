1072f4aa99a648780ae9e0d349b68c5e4d182773
aoneban <torgregion1@gmail.com>
2025-11-11 15:06:54 +0100

    feat: create appearance of modal window

--- src/app/menu-page/menu-page.component.ts
+++ src/app/menu-page/menu-page.component.ts
@@ -1,21 +1,26 @@
 /* eslint-disable @angular-eslint/prefer-inject */
 import { Component, OnInit, signal } from '@angular/core';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { HttpClient } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 import { AllProductsService } from '../all-products.service';
-import { Product } from '../interfaces';
 import { data } from '../../../public/assets/data/data';
+import { Product, Product2, ProductsResponse2 } from '../interfaces';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { ModalWindowComponent } from './modal-window/modal-window.component';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-menu-page',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule, ModalWindowComponent],
+  imports: [CommonModule, ReactiveFormsModule, MatDialogModule],
   templateUrl: './menu-page.component.html',
   styleUrl: './menu-page.component.scss',
 })
 export class MenuPageComponent implements OnInit {
+  private apiUrl = 'https://6kt29kkeub.execute-api.eu-central-1.amazonaws.com/products/';
   selectedId = signal<number | null>(null);
+  productSignal = signal<Product2 | null>(null);
   products: Product[] = [];
   categories = [
     { name: 'coffee', image: '../assets/images/coffee-tab.png' },
@@ -24,7 +29,11 @@ export class MenuPageComponent implements OnInit {
   ];
   selected = new FormControl('coffee');
 
-  constructor(private allProductsService: AllProductsService) {}
+  constructor(
+    private allProductsService: AllProductsService,
+    private http: HttpClient,
+    private dialog: MatDialog,
+  ) {}
 
   ngOnInit(): void {
     this.allProductsService.getProducts().subscribe({
@@ -53,5 +62,31 @@ export class MenuPageComponent implements OnInit {
 
   selectProduct(id: number) {
     this.selectedId.set(id);
+
+    this.getProductData(id).subscribe({
+      next: (res) => {
+        this.productSignal.set(res.data);
+        this.openDialog(res.data);
+        console.log('Product data:', res.data);
+      },
+      error: (err) => {
+        this.productSignal.set(null);
+        this.openDialog(null);
+        console.error('Error response:', err);
+      },
+    });
+  }
+
+  openDialog(product: Product2 | null): void {
+    this.dialog.open(ModalWindowComponent, {
+      data: product,
+      width: '400px',
+    });
+  }
+
+  getProductData(id: number): Observable<ProductsResponse2> {
+    return this.http.get<ProductsResponse2>(`${this.apiUrl}${id}`, {
+      headers: { accept: 'application/json' },
+    });
   }
 }
--- src/app/menu-page/modal-window/modal-window.component.ts
+++ src/app/menu-page/modal-window/modal-window.component.ts
@@ -1,57 +1,17 @@
 import { CommonModule } from '@angular/common';
-import { HttpClient } from '@angular/common/http';
-import { Component, Input, Signal, signal, effect } from '@angular/core';
-import { ProductsResponse2, Product2 } from '../../interfaces';
-import { Observable } from 'rxjs';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Component, Inject } from '@angular/core';
+import { MatDialogModule } from '@angular/material/dialog';
+import { Product2 } from '../../interfaces';
 
 @Component({
   selector: 'app-modal-window',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatDialogModule],
   templateUrl: './modal-window.component.html',
   styleUrls: ['./modal-window.component.scss'],
 })
 export class ModalWindowComponent {
-  private apiUrl = 'https://6kt29kkeub.execute-api.eu-central-1.amazonaws.com/products/';
-  private currentSignal = signal<Signal<number | null> | null>(null);
-
-  @Input()
-  set productIdSignal(s: Signal<number | null> | null) {
-    this.currentSignal.set(s ?? null);
-  }
-
-  product: Product2 | null = null;
-
   // eslint-disable-next-line @angular-eslint/prefer-inject
-  constructor(private http: HttpClient) {
-    effect((onCleanup) => {
-      const s = this.currentSignal();
-      if (!s) {
-        this.product = null;
-        return;
-      }
-
-      const id = s();
-      if (id == null) {
-        this.product = null;
-        return;
-      }
-
-      const sub = this.getProductData(id).subscribe({
-        next: (res) => {
-          this.product = res.data;
-          console.log('Product data:', this.product);
-        },
-        error: (err) => console.error('Error response:', err),
-      });
-
-      onCleanup(() => sub.unsubscribe());
-    });
-  }
-
-  getProductData(id: number): Observable<ProductsResponse2> {
-    return this.http.get<ProductsResponse2>(`${this.apiUrl}${id}`, {
-      headers: { accept: 'application/json' },
-    });
-  }
+  constructor(@Inject(MAT_DIALOG_DATA) public data: Product2) {}
 }
