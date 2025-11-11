1ccdab0c11e19bd630eaabee6bd688650658690e
aoneban <torgregion1@gmail.com>
2025-11-11 01:04:07 +0100

    feat: continue doing on modal window

--- src/app/interfaces.ts
+++ src/app/interfaces.ts
@@ -11,3 +11,36 @@ export interface Product {
 export interface ProductsResponse {
   data: Product[];
 }
+
+export interface ProductsResponse2 {
+  data: Product2;
+}
+
+export interface Product2 {
+  id: number;
+  name: string;
+  description: string;
+  price: string;
+  discountPrice?: string;
+  category: string;
+  sizes: Record<string, SizeDetail>;
+  additives: Additive[];
+}
+
+export interface SizeDetail {
+  size: string;
+  price: string;
+  discountPrice?: string;
+}
+
+export interface Sizes {
+  s?: SizeDetail;
+  m?: SizeDetail;
+  l?: SizeDetail;
+  [key: string]: SizeDetail | undefined;
+}
+
+export interface Additive {
+  name: string;
+  price: string;
+}
--- src/app/menu-page/modal-window/modal-window.component.ts
+++ src/app/menu-page/modal-window/modal-window.component.ts
@@ -1,16 +1,57 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input, OnInit, Signal } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, Input, Signal, signal, effect } from '@angular/core';
+import { ProductsResponse2, Product2 } from '../../interfaces';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-modal-window',
+  standalone: true,
   imports: [CommonModule],
   templateUrl: './modal-window.component.html',
-  styleUrl: './modal-window.component.scss',
+  styleUrls: ['./modal-window.component.scss'],
 })
-export class ModalWindowComponent implements OnInit {
-  @Input() productIdSignal!: Signal<number | null>;
+export class ModalWindowComponent {
+  private apiUrl = 'https://6kt29kkeub.execute-api.eu-central-1.amazonaws.com/products/';
+  private currentSignal = signal<Signal<number | null> | null>(null);
 
-  ngOnInit(): void {
-    console.log('Выбран продукт с id:', this.productIdSignal());
+  @Input()
+  set productIdSignal(s: Signal<number | null> | null) {
+    this.currentSignal.set(s ?? null);
+  }
+
+  product: Product2 | null = null;
+
+  // eslint-disable-next-line @angular-eslint/prefer-inject
+  constructor(private http: HttpClient) {
+    effect((onCleanup) => {
+      const s = this.currentSignal();
+      if (!s) {
+        this.product = null;
+        return;
+      }
+
+      const id = s();
+      if (id == null) {
+        this.product = null;
+        return;
+      }
+
+      const sub = this.getProductData(id).subscribe({
+        next: (res) => {
+          this.product = res.data;
+          console.log('Product data:', this.product);
+        },
+        error: (err) => console.error('Error response:', err),
+      });
+
+      onCleanup(() => sub.unsubscribe());
+    });
+  }
+
+  getProductData(id: number): Observable<ProductsResponse2> {
+    return this.http.get<ProductsResponse2>(`${this.apiUrl}${id}`, {
+      headers: { accept: 'application/json' },
+    });
   }
 }
